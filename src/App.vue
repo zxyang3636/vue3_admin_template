8d8894a5c35a8f91c558f315aaa66f7225c990d6
zxyang3636 <1753632810@qq.com>
2025-06-21 22:59:30 +0800

    feat: add element plus

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
   <div>
     <h1>我是App</h1>
-    <el-button type="success" size="default" @click="" :icon="Edit">success</el-button>
+    <el-button type="success" size="default" @click="" :icon="Edit">
+      success
+    </el-button>
 
-    <el-button type="primary" size="default" @click="" :icon="Plus">你好</el-button>
-
+    <el-button type="primary" size="default" @click="" :icon="Plus">
+      你好
+    </el-button>
   </div>
 </template>
 
 <script setup lang="ts">
-import { Plus, Edit, Delete } from '@element-plus/icons-vue';
+import { Plus, Edit, Delete } from '@element-plus/icons-vue'
 const handleClick = () => {
   console.log('aaaa')
 }
