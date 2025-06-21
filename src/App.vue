c097778ac04462a1ecee83bbbd12a471e18cdf65
zxyang3636 <1753632810@qq.com>
2025-06-21 22:59:14 +0800

    feat: add element plus

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>我是App</h1>
-    <button @click="handleClick"></button>
+    <el-button type="success" size="default" @click="" :icon="Edit">success</el-button>
+
+    <el-button type="primary" size="default" @click="" :icon="Plus">你好</el-button>
+
   </div>
 </template>
 
 <script setup lang="ts">
+import { Plus, Edit, Delete } from '@element-plus/icons-vue';
 const handleClick = () => {
   console.log('aaaa')
 }
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+//@ts-ignore忽略当前文件ts类型的检测否则有红色提示(打包会失败)
+import zhCn from 'element-plus/dist/locale/zh-cn.mjs'
 
-createApp(App).mount('#app')
-
-const fun = () => {
-  console.log('aaa')
-}
-
-fun()
+const app = createApp(App)
+app.use(ElementPlus, {
+  locale: zhCn,
+})
+app.mount('#app')
